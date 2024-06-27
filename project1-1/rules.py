589192cdda2be0cef8fc001cb1baf48b97ace419
jatesun <jatesun111@gmail.com>
2024-06-27 16:28:08 +0800

    2024年06月27日 rules新增。

--- project1-1/rules.py
+++ project1-1/rules.py
@@ -1,7 +1,75 @@
-class Rules:
+class Rule:
     """
     规则基类
     """
 
-    def action(self, block, hander):
-        pass
+    def action(self, block, handler):
+        handler.start(self.type)
+        handler.feed(block)
+        handler.end(self.type)
+        return True
+
+
+class HeadingRule(Rule):
+    """
+    标题只包含一行，不超过70个字符且不以冒号结尾
+    """
+    type = 'heading'
+
+    def condition(self, block):
+        return not '\n' in block and len(block) <= 70 and not block[-1] == ':'
+
+
+class TitleRule(HeadingRule):
+    type = 'title'
+    first = True
+
+    def condition(self, block):
+        if not self.first:
+            return False
+        self.first = False
+        return HeadingRule.condition(self, block)
+        # return super().condition(block)
+
+
+class ListItemRule(Rule):
+    type = 'listitem'
+    """
+    列表项是-开头的段落，设置的时候去除-
+    """
+
+    def condition(self, block):
+        return block[0] == '-'
+
+    def action(self, block, handler):
+        handler.start(self.type)
+        handler.feed(block[1:].strip())
+        handler.end(self.type)
+        return True
+
+
+class ListRule(ListItemRule):
+    type = 'list'
+    inside = False
+
+    def condition(self, block):
+        return True
+
+    def action(self, block, handler):
+        if not self.inside and ListItemRule.condition(self, block):
+            handler.start(self.type)
+            self.inside = True
+        elif self.inside and not ListItemRule.condition(self, block):
+            handler.end(self.type)
+            self.inside = False
+        return False
+
+
+class ParagraphRule(Rule):
+    """
+    段落是不符合其他规则的文本
+    """
+    type = "paragraph"
+
+    def condition(self, block):
+        return True
